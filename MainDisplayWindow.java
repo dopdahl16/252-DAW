9ac147a1562f4eef97a172483f0ab1edb88cffca
Daniel Opdahl <dopdahl16@gmail.com>
2018-11-30 12:57:45 -0600

    Update MainDisplayWindow.java

--- MainDisplayWindow.java
+++ MainDisplayWindow.java
@@ -27,10 +27,17 @@ public class MainDisplayWindow extends JPanel {
     
 	public ProgramFrame program_frame;
 	public ArrayList<File> tracks_list;
+	public File track;
+	public int track_index;
+	public int current_track;
+	//	track_index is for creating the audio files in AudioDisplayContainer,AudioFileInfo, and AudioFileVisualDisplay only
+	//	current_track is for playback purposes ONLY
 	
-    public MainDisplayWindow(ProgramFrame program_frame, ArrayList<File> tracks_list) {
+    public MainDisplayWindow(ProgramFrame program_frame, ArrayList<File> tracks_list, int current_track) {
         
     	setTracksList(tracks_list);
+    	setProgramFrame(program_frame);
+    	setCurrentTrack(current_track);
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         setBorder(BorderFactory.createLineBorder(Color.black));
         
@@ -53,13 +60,19 @@ public class MainDisplayWindow extends JPanel {
        
     }
     
-    void addAudioFile(ArrayList<File> tracks_list) {
+    void addAudioFile(int track_index) {
     	
-    	AudioDisplayContainer new_container = new AudioDisplayContainer(getTracksList());
+    	setTrack(track_index);
+    	AudioDisplayContainer new_container = new AudioDisplayContainer(this, getTrack());
     	new_container.setMaximumSize(new Dimension(Integer.MAX_VALUE, 250));
     	add(new_container);
     	revalidate();
     	repaint();
+    }
+    
+    void selectTrack(File selected_track) {
+    	setCurrentTrack(getTracksList().indexOf(selected_track));
+    	System.out.println("Track: " + selected_track.getName() + " selected");
     }
     
     void setProgramFrame(ProgramFrame other) {
@@ -76,5 +89,18 @@ public class MainDisplayWindow extends JPanel {
     void setTracksList(ArrayList<File> other){
         tracks_list = other;
     }
-
+    File getTrack(){
+        return track;
+    }
+    
+    void setTrack(int track_index){
+        track = getTracksList().get(track_index);
+    }
+    int getCurrentTrack(){
+        return current_track;
+    }
+    
+    void setCurrentTrack(int other){
+        current_track = other;
+    }
 }
