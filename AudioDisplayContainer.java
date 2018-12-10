476c613cf19ebdb7ca909c6fb71301170c548093
Daniel Opdahl <dopdahl16@gmail.com>
2018-12-10 17:01:27 -0600

    Update AudioDisplayContainer.java
    The correct class. Dunno what was here before

--- AudioDisplayContainer.java
+++ AudioDisplayContainer.java
@@ -1,72 +1,70 @@
+package daw;
 
-    	
-    	File current_file = getTracksList().get(getCurrentTrack());
-    	File write_file = new File("C:\\Users\\dopda\\Desktop\\DAW WAV Files\\" + "1" + getTracksList().get(getCurrentTrack()).getName());
-    	
-    	FileOutputStream out = null;
-    	FileInputStream in = null;
-    	
-    	
-		try {
-			in = new FileInputStream(current_file);
-			out = new FileOutputStream(write_file);
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		
-		
-		
-		
-		try {
-			for (int i = 0; i<(current_file.length()/2); i++) {
-				
-				while (i < 44) {
-					byte b1 = (byte) (in.read() & 0xff);
-					byte b2 = (byte) (in.read() & 0xff);
-					System.out.println(b1 + " " + b2 + " ");
-					out.write(b1);
-					out.write(b2);
-					System.out.println(b1 + " " + b2 + " ");
-					System.out.println(i);
-					i++;
-				}
-				
-				
-			byte b1 = (byte) (in.read() & 0xff);
-			byte b2 = (byte) (in.read() & 0xff);
-			
-			short s = ((short) (b2 << 8 | (((int) b1) & 0xff) & 0xffff));
-			
-				s = (short) (s * scaling_ratio);
-				byte b3 = ((byte) (s & 0xff));
-				byte b4 = ((byte) (s >> 8 & 0xff));
-				if (b1 * scaling_ratio >= 127 || b1 * scaling_ratio <= -128) {
-					if (b1 > 0) {
-						b3 = 127;
-					}
-					if (b1 < 0) {
-						b3 = -128;
-					}
-				}
-				if (b2 * scaling_ratio >= 127 || b2 * scaling_ratio <= -128) {
-					if (b2 > 0) {
-						b4 = 127;
-					}
-					if (b2 < 0) {
-						b4 = -128;
-					}
-				}
-			
-				out.write(b3);
-				out.write(b4);
-			
-			}
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.File;
+
+/**
+ *
+ * @author opdada01 Daniel Opdahl
+ * @author mantno01 Noah Manternach
+ * @author nteste01 Teboho Nteso
+ * 
+ */
+
+//The AudioDisplayContainer class serves as a container component for AudioFileInfo objects and 
+//AudioFileVisualDisplay objects.
+
+public class AudioDisplayContainer extends JPanel {
     
+	/* FIELDS */
+	
+	public File track;
+	public MainDisplayWindow main_display_window;
+	
+	
+	/* CONSTRUCTOR */
+	
+    public AudioDisplayContainer(MainDisplayWindow main_display_window, File track) {
+        
+    	//The AudioDisplayContainer constructor is given a main_display_window and a track. After
+    	//setting the track, main_display_window for functional purposes and layout, border and
+    	//minimumsize for display and astetic purposes, we create and add the AudioFileVisualDisplay
+    	//and AudioFileInfo objects for our respective track.
+    	setTrack(track);
+    	setMainDisplayWindow(main_display_window);
+        setLayout(new BorderLayout());
+        setBorder(BorderFactory.createLineBorder(Color.green));
+        setMinimumSize(new Dimension(500,10));
+        
+        AudioFileVisualDisplay audio_file_display = new AudioFileVisualDisplay();
+        AudioFileInfo audio_file_info = new AudioFileInfo(this, getMainDisplayWindow(), getTrack());
+        
+        add(audio_file_info, BorderLayout.LINE_START);
+        add(audio_file_display, BorderLayout.LINE_END);
+
+    }
+
+    
+    /* ACCESSORS */
+    
+    File getTrack() {
+        return track;
+    }
+    
+    MainDisplayWindow getMainDisplayWindow() {
+        return main_display_window;
+    }
+    
+    
+    /* MUTATORS */
+    
+    void setTrack(File other) {
+        track = other;
+    }
+    
+    void setMainDisplayWindow(MainDisplayWindow other) {
+        main_display_window = other;
+    }
+}
