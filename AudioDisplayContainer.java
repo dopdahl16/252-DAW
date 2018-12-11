f564375d778ff32871dd988ae917e7fd2a1f8776
Noah Manternach <mantno01@luther.edu>
2018-12-10 21:47:34 -0600

    Update AudioDisplayContainer.java

--- AudioDisplayContainer.java
+++ AudioDisplayContainer.java
@@ -2,21 +2,9 @@ package daw;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.geom.*;
-import java.util.*;
-import java.util.*;
 import javax.swing.*;
-import java.awt.EventQueue;
-import java.awt.event.KeyEvent;
 import java.io.File;
-import java.io.Serializable;
-import java.net.URL;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.sound.sampled.*;
+
 
 /**
  *
@@ -26,14 +14,14 @@ import javax.sound.sampled.*;
  * 
  */
 
-/*
-DOCUMENTATION
-*/
-
 public class AudioDisplayContainer extends JPanel implements ActionListener {
+    
+    //FIELDS//
     
 	public File track;
 	public MainDisplayWindow main_display_window;
+        
+    //CONSTRUCTOR//
 	
     public AudioDisplayContainer(MainDisplayWindow main_display_window, File track) {
         
@@ -44,7 +32,7 @@ public class AudioDisplayContainer extends JPanel implements ActionListener {
         setMinimumSize(new Dimension(500,10));
         
         AudioFileVisualDisplay audio_file_display = new AudioFileVisualDisplay();
-        AudioFileInfo audio_file_info = new AudioFileInfo(this,getMainDisplayWindow(), getTrack());
+        AudioFileInfo audio_file_info = new AudioFileInfo(this, getMainDisplayWindow(), getTrack());
         
         add(audio_file_info, BorderLayout.LINE_START);
         add(audio_file_display, BorderLayout.LINE_END);
@@ -57,16 +45,22 @@ public class AudioDisplayContainer extends JPanel implements ActionListener {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    //ACCESSORS//
+    
     File getTrack(){
         return track;
     }
     
-    void setTrack(File other){
-        track = other;
-    }
     MainDisplayWindow getMainDisplayWindow(){
         return main_display_window;
     }
+    
+    //MUTATORS//
+    
+    void setTrack(File other){
+        track = other;
+    }
+    
     
     void setMainDisplayWindow(MainDisplayWindow other){
         main_display_window = other;
